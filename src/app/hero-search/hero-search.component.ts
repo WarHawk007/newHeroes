4fd89ae1990e942aa41a0c9939780b71e104cf67
farhansaeed <farhansaeed27@gmail.com>
2018-09-28 00:33:50 +0500

    Done

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import {HeroesdataService} from '../heroesdata.service';
 
 @Component({
   selector: 'app-hero-search',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSearchComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[];
+  sheroes: Hero[];
+  temp: string;
+  searchForm = this.fb.group({
+    search: ['', Validators.required]
+  });
+  constructor(private herodata: HeroesdataService , private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.herodata.getHeroes().subscribe(data => this.heroes = data);
   }
+  getsearchHeroes() {
+    if (this.searchForm.get('search').value !== '') {
+      this.sheroes = null;
 
+      this.sheroes = this.heroes.filter(shero => {
+            return shero.name.toLowerCase().includes(this.searchForm.get('search').value.toLowerCase());
+           });
+      console.log('Searched Heroes' , this.sheroes);
+      console.log('All Heroes' , this.heroes);
+    } else {
+      this.sheroes = null;
+    }
+  }
 }
--- src/app/heroesdata.service.ts
+++ src/app/heroesdata.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Hero} from './hero';
 import {Observable} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesdataService {
-
   constructor(private httpclient: HttpClient) {}
   getHeroes(): Observable<Hero[]> {
+    // return this.httpclient.get<Hero[]>('https://modernlibrarysystem.000webhostapp.com/heroapi/herodata.php');
     return this.httpclient.get<Hero[]>('http://localhost/heroapi/herodata.php');
   }
   addHero(hero: Hero): Observable<Hero> {
-    console.log('Something Happened');
-    return this.httpclient.post<Hero>('http://localhost/heroapi/inserthero.php', hero );
+    return this.httpclient.post<Hero>('http://localhost/heroapi/inserthero.php', hero);
   }
   getHero(heroid): Observable<Hero> {
     return this.httpclient.get<Hero>('http://localhost/heroapi/herodata.php?id=' + heroid);
@@ -23,5 +22,8 @@ export class HeroesdataService {
   }
   deleteHero(hero: Hero): Observable<Hero> {
     return this.httpclient.delete<Hero>('http://localhost/heroapi/deletehero.php?id=' + hero.id);
+  }
+  searchbyname(name): Observable<Hero[]> {
+    return this.httpclient.get<Hero[]>('http://localhost/heroapi/herodata.php?name=' + name);
   }
 }
